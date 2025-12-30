49df71c67d2b70098f271a71ac3c90003dfd17fd
imed <imededdine.benzarti@gmail.com>
2025-12-31 00:38:31 +0100

    firdt commit

---
+++ src/test/java/com/attendanceserver/AttendanceserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendanceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
